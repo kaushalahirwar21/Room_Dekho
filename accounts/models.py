0d351b6bc93b984fe26787fbbabcd6b542224d9d
KAUSHAL SINGH AHIRWAR <kaushalahirwar714@gmail.com>
2026-06-29 01:55:02 +0530

    Update OTP otp_code max_length to 64 and generate migrations

--- accounts/models.py
+++ accounts/models.py
@@ -54,7 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class OTP(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    otp_code = models.CharField(max_length=6)
+    otp_code = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
     attempts = models.IntegerField(default=0)
 
